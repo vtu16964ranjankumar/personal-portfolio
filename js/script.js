ed36b00877251689179ee01d0de6b3c18fd32b53
Ranjan Kumar <111798933+vtu16964ranjankumar@users.noreply.github.com>
2023-09-02 00:13:35 +0530

    In progress

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// ================= typing Animation ===========================
+var typed = new typed(".typing", {
+    strings: ["", "Web Develepor", "Student", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+
+})
